ad6a2787da60570cc888f0199278bb5d7d362903
Moskvitin Matwey <matweima.moskva@yandex.ru>
2022-12-27 12:16:37 +0500

    л

--- Python/Converter.py
+++ Python/Converter.py
@@ -1,6 +1,8 @@
-def ten_to_binary(val):
+import time
+
+def decimal_to_binary(val):
     nums = []
-    while True:           #Цикл делит число на два и записывает в список единицы и нули
+    while True:           
         if val == 1:
             nums.append(1)
             break
@@ -12,15 +14,49 @@ def ten_to_binary(val):
             nums.append(1)
             val = val // 2
             continue         
-    nums.reverse()        #Переставляет единицы и нули в списке в обратном порядке
+    nums.reverse()        
     result = str(nums)               
-    result = result.replace(", ", "")  #Удаление ненужных символов
+    result = result.replace(", ", "")  
     result = result.replace("[", "")
     result = result.replace("]", "")
     return int(result)
 
-print('Конвертер двоичных чисел')
-result = ten_to_binary(int(input("Введите целое десятичное число -> ")))
-print('Результат:')
-print(result)
-exit = input()
+def binary_to_decimal(val):
+    val = str(val)[::-1]
+    x = 0
+    result = 0
+    for i in val:
+        result += int(i) * (2**x)
+        x+=1
+    return result
+    
+
+def menu():
+    print("Конвертер двоичных чисел by Worm787898 https://github.com/Worm787898")
+    print("-----------------------------------------------------------------------")
+    time.sleep(0.5)
+    while True:
+        print("Выберите режим:")
+        print("1 - Десятичное в двоичное")
+        print("2 - Двоичное в десятичное")
+        print("3 - Выход")
+        time.sleep(0.5)
+        choose = input(" -> ")
+        try:
+            if choose == "1":
+                number = int(input("Введите целое десятичное число -> "))
+                print("Результат: " + str(decimal_to_binary(number)))
+            elif choose == "2":
+                number = int(input("Введите целое двоичное число -> "))
+                print("Результат: " + str(binary_to_decimal(number)))
+            elif choose == "3":
+                break
+            else:
+                print("Неверный ввод")
+        except:
+            print("Неверный ввод")
+        time.sleep(1.5)
+        print("------------------------------------")
+        time.sleep(1)    
+
+menu()
